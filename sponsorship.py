65cfbd695d6204b540a56e8e2b495dae74aceabf
lucemia <lucemia@gmail.com>
2015-05-24 20:47:34 +0800

    connect to server

--- sponsorship.py
+++ sponsorship.py
@@ -1,7 +1,22 @@
 from lib.rfid_mfrc522 import read_rfid
 from lib import info
+import requests
+import logging
+
+api = 'http://codeme.krdai.info/api/checkin/'
 
 print info.MACHINE_ID, info.SD_ID
 
 for index, uid in enumerate(read_rfid()):
     print index, uid
+
+    try:
+        r = requests.post(api, {
+            'rfid': uid,
+            'mid': info.MACHINE_ID,
+        })
+        assert r.status_code == requests.codes.ok, r.text
+    except Exception as e:
+        logging.exeption(e)
+
+    # beep!!
