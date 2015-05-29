d2ea31c0d4f87f13e0c73a63ad5d0e13c1837400
Bruce Tsai <wagamama.tsai@gmail.com>
2015-05-29 22:44:20 +0800

    Add 'regist_wtime" and 'tshirt_wtime' in query return

--- server.php
+++ server.php
@@ -27,18 +27,18 @@ if ( isset($_POST))
                     $result = array(
                         'status' => 'ok',
                         'result' => array('reg_no' => $r[0]['reg_no'],
-                                        'nickname' => $r[0]['nickname'], 
-                                        'tshirt'   => $r[0]['tshirt'], 
+                                        'nickname' => $r[0]['nickname'],
+                                        'tshirt'   => $r[0]['tshirt'],
                                         'tshirt_wtime' => $r[0]['tshirt_wtime']),
                     );
                 }
-                else 
+                else
                 {
                     $result = array(
                         'status' => 'fail',
-                        'result' => array('reg_no' => $err['not_found'], 
+                        'result' => array('reg_no' => $err['not_found'],
                                         'nickname' => $err['not_found'],
-                                        'tshirt'   => $err['not_found'], 
+                                        'tshirt'   => $err['not_found'],
                                         'tshirt_wtime' => $err['not_found'])
                     );
                 }
@@ -49,15 +49,15 @@ if ( isset($_POST))
             {
                 $result = array(
                     'status' => 'fail',
-                    'result' => array('reg_no' => $err['no_uid_send'], 
+                    'result' => array('reg_no' => $err['no_uid_send'],
                                     'nickname' => $err['nickname'],
-                                    'tshirt'   => $err['no_uid_send'], 
+                                    'tshirt'   => $err['no_uid_send'],
                                     'tshirt_wtime' => $err['no_uid_send'])
                 );
 
                 echo json_encode($result);
             }
-            
+
         break;
 
         case 'tshirt-update':
@@ -88,8 +88,8 @@ if ( isset($_POST))
                 else
                 {
                     $result = array('status' => 'fail',
-                                    'result' => array('reg_no' => $err['no_such_uid'], 
-                                                    'tshirt' => $err['no_such_uid'], 
+                                    'result' => array('reg_no' => $err['no_such_uid'],
+                                                    'tshirt' => $err['no_such_uid'],
                                                     'tshirt_wtime' => $err['no_such_uid'])
                                 );
 
@@ -118,7 +118,7 @@ if ( isset($_POST))
                 if ( count($r) == 0)
                 {
                     $result = array('status' => 'fail',
-                                    'result' => array('reg_no' => $_POST['reg_no'],  
+                                    'result' => array('reg_no' => $_POST['reg_no'],
                                                     'uid' => 'NOT FOUND!',
                                                     'nickname' => 'NOT FOUND!')
                                     );
@@ -154,7 +154,9 @@ if ( isset($_POST))
                     $result = array('status' => 'fail',
                                     'result' => array('reg_no' => 'NOT FOUND!',
                                                     'uid' => $_POST['uid'],
-                                                    'nickname' => 'NOT FOUND!')
+                                                    'nickname' => 'NOT FOUND!',
+                                                    'regist_wtime' => 'NOT FOUND',
+                                                    'tshirt_wtime' => 'NOT FOUND')
                                     );
                 }
                 elseif ( count($r) == 1)
@@ -162,13 +164,15 @@ if ( isset($_POST))
                     $result = array('status' => 'ok',
                                     'result' => array('reg_no' => $r[0]['reg_no'],
                                                     'uid' => $r[0]['uid'],
-                                                    'nickname' => $r[0]['nickname'])
+                                                    'nickname' => $r[0]['nickname'],
+                                                    'regist_wtime' => $r[0]['regist_wtime'],
+                                                    'tshirt_wtime' => $r[0]['tshirt_wtime'])
                                     );
                 }
                 else
                 {
-                    $str = ''; 
-                    foreach ($r as $v) 
+                    $str = '';
+                    foreach ($r as $v)
                     {
                         $str = $str . $v['reg_no'] . ", ";
                     }
@@ -176,7 +180,9 @@ if ( isset($_POST))
                     $result = array('status' => 'fail',
                                     'result' => array('reg_no' => 'OVER ONE RECORD!',
                                                     'uid' => $_POST['uid'],
-                                                    'nickname' => $str)
+                                                    'nickname' => $str,
+                                                    'regist_wtime' => 'OVER ONE RECORD!',
+                                                    'tshirt_wtime' => 'OVER ONE RECORD!')
                                     );
                 }
 
