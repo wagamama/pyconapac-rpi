e4bbfed9dd3e36ff020c81a6583c3578a6998497
lucemia <lucemia@gmail.com>
2015-05-31 13:02:46 +0800

    set buzz pin

--- lib/buzzer.py
+++ lib/buzzer.py
@@ -2,10 +2,11 @@ import RPi.GPIO as GPIO
 import time
 
 class BuzzManager:
+    buzzer_pin = 11
+
     def __enter__(self):
-        buzzer_pin = 11
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(buzzer_pin, GPIO.OUT)
+        GPIO.setup(self.buzzer_pin, GPIO.OUT)
         return self
 
     def buzz(self, pitch=800, duration=0.1):
@@ -13,9 +14,9 @@ class BuzzManager:
         delay = period / 2
         cycles = int(duration * pitch)
         for i in range(cycles) :
-            GPIO.output(buzzer_pin, True)
+            GPIO.output(self.buzzer_pin, True)
             time.sleep(delay)
-            GPIO.output(buzzer_pin, False)
+            GPIO.output(self.buzzer_pin, False)
             time.sleep(delay)
 
     def __exit__(self, type, value, traceback):
