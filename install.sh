6a5c8f9020ca046fbe3ceaef2b78a9e7b6d9c32d
lucemia <lucemia@gmail.com>
2015-06-02 20:26:20 +0800

    update install script

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
+cd /home/pi
 wget https://github.com/wagamama/pyconapac-rpi/archive/master.zip
+rm -rf /home/pi/pyconapac-rpi
+unzip master.zip
+mv /home/pi/pyconapac-rpi-master /home/pi/pyconapac-rpi
+touch /tmp/network
