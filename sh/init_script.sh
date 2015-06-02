253245cb7a8de2b89d8e11ef775350cbcbc6ae33
sosorry <sosorry@raspberrypi.com.tw>
2015-06-02 20:35:02 +0800

    update the latest code while online

---
+++ sh/init_script.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [ -f /tmp/network ]; then
+    curl https://raw.githubusercontent.com/wagamama/pyconapac-rpi/master/install.sh | sh
+    cd /home/pi/pyconapac-rpi
+    sudo python sponsorship.py &
+fi
