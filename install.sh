f9f05d3f6717aa47f631d0eb022981b68425fd4a
sosorry <sosorry@raspberrypi.com.tw>
2015-06-04 00:21:04 +0800

    recover install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ ! -f /tmp/network ]; then
-    i="0"
-    while [ $i -lt 4 ]
-    do
-        cd /home/pi
-        rm -f /home/pi/master.zip
-        wget https://github.com/wagamama/pyconapac-rpi/archive/master.zip
-        size=`ls -al master.zip | awk '{print $5}'`
-        if [ "$size" -gt 10000 ]; then
-            rm -rf /home/pi/pyconapac-rpi
-            unzip master.zip
-            mv /home/pi/pyconapac-rpi-master /home/pi/pyconapac-rpi
-            chown -R pi:pi pyconapac-rpi
-            touch /tmp/network
-            cd /home/pi/pyconapac-rpi
-            # put check bee here
-
-            sudo python /home/pi/pyconapac-rpi/sponsorship.py &
-            python /home/pi/pyconapac-rpi/health_check.py &
-            break
-        fi
-        i=$[$i+1]
-    done
+    cd /home/pi
+    rm -f /home/pi/master.zip
+    wget https://github.com/wagamama/pyconapac-rpi/archive/master.zip
+    size=`ls -al master.zip | awk '{print $5}'`
+    if [ "$size" -gt 10000 ]; then
+        rm -rf /home/pi/pyconapac-rpi
+        unzip master.zip
+        mv /home/pi/pyconapac-rpi-master /home/pi/pyconapac-rpi
+        chown -R pi:pi pyconapac-rpi
+        touch /tmp/network
+        cd /home/pi/pyconapac-rpi
+        i="0"
+        while [ $i -lt 4 ] 
+        do
+            sudo python /home/pi/pyconapac-rpi/lib/buzzer.py
+            let i=$i+1
+        done
+        sudo kill -9 $(ps -ef | grep sponsorship.py | awk '{print $2}')
+        sudo python /home/pi/pyconapac-rpi/sponsorship.py &
+    fi  
 fi
-
-
