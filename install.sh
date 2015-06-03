823470dabc520773b91b29eaae09100761082dd0
lucemia <lucemia@gmail.com>
2015-06-04 03:32:14 +0800

    update install

--- install.sh
+++ install.sh
@@ -11,12 +11,9 @@ if [ ! -f /tmp/network ]; then
         mv /home/pi/pyconapac-rpi-master /home/pi/pyconapac-rpi
         chown -R pi:pi pyconapac-rpi
         cd /home/pi/pyconapac-rpi
-        i="0"
-        while [ $i -lt 4 ]
-        do
-            sudo python /home/pi/pyconapac-rpi/lib/buzzer.py
-            let i=$i+1
-        done
+
+        sudo python /home/pi/pyconapac-rpi/lib/buzzer.py
+
         sudo dpkg --configure -a
         sudo apt-get install python-pip
         sudo pip install -r /home/pi/pyconapac-rpi/requirements.txt
@@ -24,5 +21,13 @@ if [ ! -f /tmp/network ]; then
         sudo python /home/pi/pyconapac-rpi/sponsorship.py &
         sudo python /home/pi/pyconapac-rpi/health_check.py &
         touch /tmp/network
+
+        i="0"
+        while [ $i -lt 4 ]
+        do
+            sudo python /home/pi/pyconapac-rpi/lib/buzzer.py
+            let i=$i+1
+        done
+
     fi
 fi
